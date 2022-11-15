8e92dbfab9bbc842b9a7e125ca0c4158e4917f53
Matias Pinto <pintomatias2@gmail.com>
2022-11-15 13:32:29 -0300

    firsts routes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const postsRoutes = require('./routes/posts.routes');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(postsRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Conectado a ${PORT}!`)
+});
---
+++ src/routes/posts.routes.js
@@ -0,0 +1,20 @@
+const { Router, application } = require('express');
+const router = Router();
+
+router.get('/posts', (req, res) => {
+    res.send('GET posts');
+});
+
+router.get('/posts/10', (req, res) => {
+    res.send('GET one post');
+});
+
+router.post('/posts', (req, res) => {
+    res.send('POST post');
+});
+
+router.delete('/posts', (req, res) => {
+    res.send('DELETE post');
+});
+
+module.exports = router;
