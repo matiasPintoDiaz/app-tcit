12541c7646dd58d148de6b9b56db0e9cb99edb3d
Matias Pinto <pintomatias2@gmail.com>
2022-11-16 19:31:17 -0300

    archivo .env

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const postsRoutes = require("./routes/posts.routes");
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(postsRoutes);
 
