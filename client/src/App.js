db94f55b39240adce2ebc6f184913960ce1afecb
Matias Pinto <pintomatias2@gmail.com>
2022-11-17 02:55:12 -0300

    Firsts Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormPosts from "./components/FormPosts";
+import ListPosts from './components/ListPosts';
 
 export default function App() {
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ListPosts />}></Route>
+        <Route path='/posts/crearPost' element={<FormPosts />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 
 /* import logo from './logo.svg';
 import './App.css';
